720e94bbfed052b5406fd7cdd480d5eb79856e44
Kristian Andersson <kriax@me.com>
2014-10-22 22:30:30 +0200

    Added Nadias changes to the blog

--- src/java/no/uia/slit/nadia/BlogBean.java
+++ src/java/no/uia/slit/nadia/BlogBean.java
@@ -9,6 +9,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+import no.uia.slit.web.JsfUtils;
 
 /**
  *
@@ -18,26 +19,42 @@ import javax.inject.Named;
 @SessionScoped
 public class BlogBean implements Serializable {
     
+    private String blogText;
+
+    public String getBlogText() {
+        return blogText;
+    }
+
+    public void setBlogText(String blogText) {
+        this.blogText = blogText;
+    }
+       
 
 private ArrayList<BlogEntry> blogs;
 public BlogBean(){
-        
-        
-    }
-    
+}
     
   public ArrayList<BlogEntry> getBlogs(){
       
       blogs = new ArrayList<>();
-      
+     
       
       blogs.add(new BlogEntry(1,"Jeg skriver om Module 1...","15.10.14 kl.13:00",1));
       blogs.add(new BlogEntry(2,"Jeg skriver om Module 2...","12.11.14 kl.18:00",1));
       blogs.add(new BlogEntry(3,"Jeg skriver om Module 3...","10.12.14 kl.13:00",1));
       
-
     return blogs;
             
+  }
+  
+  public void submitBlogEntry(){
+  
+       BlogEntry blogEntry = new BlogEntry(4,blogText, "Tid",1);
+      
+       
+  
+      JsfUtils.addMessage("blogForm:submit",blogEntry.getBlogText());
+      
   }
     
 }
--- src/java/no/uia/slit/nadia/BlogEntry.java
+++ src/java/no/uia/slit/nadia/BlogEntry.java
@@ -12,7 +12,7 @@ import javax.inject.Named;
  */
 public class BlogEntry {
     private int id;
-    private String blog;
+    private String blogText;
     private String calendar;
     private int studentid;
 
@@ -21,9 +21,9 @@ public class BlogEntry {
     }
    
 
-    public BlogEntry(int id, String blog, String calendar, int studentid){
+    public BlogEntry(int id, String blogText, String calendar, int studentid){
         this.id = id;
-        this.blog = blog;
+        this.blogText = blogText;
         this.calendar = calendar;
         this.studentid = studentid;
     }
@@ -37,13 +37,13 @@ public class BlogEntry {
         this.id = id;
     }
 
-    public String getBlog() {
-        return blog;
+    public String getBlogText() {
+        return blogText;
     }
 
    
-    public void setBlog(String blog) {
-        this.blog=blog;
+    public void setBlogText(String blogText) {
+        this.blogText=blogText;
     }
     
 
